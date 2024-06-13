364ac60892e940eaa6a156042e5300988732be2c
PeterFarkas22 <farkas.peter.21f@szlgbp.hu>
2024-06-13 11:13:48 +0200

    őpopop

--- led_control/led_control/urls.py
+++ led_control/led_control/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from control.views import index, turn_on, turn_off, motor
+from control.views import index, turn_on, turn_off
 
 urlpatterns = [
     path('', index, name='index'),
     path('turn-on/', turn_on, name='turn_on'),
     path('turn-off/', turn_off, name='turn_off'),
-    path('motor/', motor, name='motor'),
 ]
