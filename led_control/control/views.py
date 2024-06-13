02991ea5607c3a7eb2ee404b7b97dd9cd7f537dd
PeterFarkas22 <farkas.peter.21f@szlgbp.hu>
2024-06-13 11:10:37 +0200

    hoi

--- led_control/control/views.py
+++ led_control/control/views.py
@@ -1,21 +1,27 @@
-from django.shortcuts import render
 import gpiod
+from django.shortcuts import render
 from gpiozero import AngularServo
+
+# Állíts be különböző GPIO pineket a LED és a szervo számára
+LED_GPIO_PIN = 17
+SERVO_GPIO_PIN = 18
 
 def set_gpio_value(chip_name, line_number, value):
     chip = gpiod.Chip(chip_name)
     line = chip.get_line(line_number)
-    line.request(consumer="gpioset", type=gpiod.LINE_REQ_DIR_OUT)
-    line.set_value(value)
-    line.release()
+    try:
+        line.request(consumer="gpioset", type=gpiod.LINE_REQ_DIR_OUT)
+        line.set_value(value)
+    finally:
+        line.release()
 
 def index(request):
     return render(request, 'index.html')
 
 def turn_on(request):
     try:
-        set_gpio_value('gpiochip4', 17, 1)
-        message = "LED off"
+        set_gpio_value('gpiochip4', LED_GPIO_PIN, 1)
+        message = "LED bekapcsolva"
     except Exception as e:
         message = f"Hiba: {e}"
     
@@ -23,20 +29,18 @@ def turn_on(request):
 
 def turn_off(request):
     try:
-        set_gpio_value('gpiochip4', 17, 0)
-        message = "LED on"
+        set_gpio_value('gpiochip4', LED_GPIO_PIN, 0)
+        message = "LED kikapcsolva"
     except Exception as e:
         message = f"Hiba: {e}"
     
     return render(request, 'index.html', {'message': message})
 
-servo = AngularServo(17, min_pulse_width=0.0006, max_pulse_width=0.0023)
+servo = AngularServo(SERVO_GPIO_PIN, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
 def motor(request):
-    template='index.html'
-    context={}
-    
-    
+    template = 'index.html'
+    context = {}
     
     if request.method == 'POST':
         angle = request.POST.get('angle')
